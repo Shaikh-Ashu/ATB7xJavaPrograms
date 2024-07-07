a0c86ffde960d220c0bbae137cf610756a865db9
Shaikh-Ashu <shaikhashapak.shaikh@gmail.com>
2024-07-07 14:29:31 +0530

    Code Added

--- src/ATB7xLearnignJava/Lab001_FirstProgram.java
+++ src/ATB7xLearnignJava/Lab001_FirstProgram.java
@@ -2,7 +2,6 @@ package ATB7xLearnignJava;
 
 public class Lab001_FirstProgram {
     public static void main(String[] args) {
-        static void Fun1();
         
         System.out.println("My Name Is Shaikh AshpaK");
 
