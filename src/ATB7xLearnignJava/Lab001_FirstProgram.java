d1176907c5d5ddf687d9f8745670a95462b6b57a
Shaikh-Ashu <shaikhashapak.shaikh@gmail.com>
2024-07-07 14:27:57 +0530

    Update Lab001_FirstProgram.java
    Added indirectly GitHub

--- src/ATB7xLearnignJava/Lab001_FirstProgram.java
+++ src/ATB7xLearnignJava/Lab001_FirstProgram.java
@@ -2,6 +2,10 @@ package ATB7xLearnignJava;
 
 public class Lab001_FirstProgram {
     public static void main(String[] args) {
+        static void Fun1();
+        
         System.out.println("My Name Is Shaikh AshpaK");
+
+        
     }
 }
