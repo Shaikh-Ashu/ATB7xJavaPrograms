2597623268b300489fe017f04a73f9fd085b63c9
Shaikh-Ashu <shaikhashapak.shaikh@gmail.com>
2024-07-20 21:47:27 +0530

    Code Added

---
+++ src/ATB7xLearnignJava/Ashu.java
@@ -0,0 +1,16 @@
+package ATB7xLearnignJava;
+
+public class Ashu extends Simmu{
+
+    static void Area1()
+    {
+        System.out.println("Area1 is running");
+    }
+
+    public static void main(String [] args)
+    {
+        Ashu.Area1();
+        Ashu.Nokia();
+        Ashu.Samsung();
+    }
+}
---
+++ src/ATB7xLearnignJava/Simmu.java
@@ -0,0 +1,13 @@
+package ATB7xLearnignJava;
+class Simmu
+{
+    static void Nokia()
+    {
+        System.out.println(100/5);
+    }
+    static void Samsung()
+    {
+        System.out.println(50+1);
+    }
+
+}
